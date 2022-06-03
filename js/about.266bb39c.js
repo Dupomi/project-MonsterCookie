a770ebf28b441a2cba992ed347e376e0599e6371
Dupomi <e238499@gmail.com>
2022-06-04 03:45:58 +0800

    deploy

---
+++ js/about.266bb39c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkmonster_cookie"]=self["webpackChunkmonster_cookie"]||[]).push([[443],{381:function(n,e,t){t.r(e),t.d(e,{default:function(){return k}});var o=t(252);const r={class:"about"},s=(0,o._)("h1",null,"This is an about page",-1),u=[s];function a(n,e){return(0,o.wg)(),(0,o.iD)("div",r,u)}var c=t(744);const i={},f=(0,c.Z)(i,[["render",a]]);var k=f}}]);
+//# sourceMappingURL=about.266bb39c.js.map
